fb342ca98ef86a7388889e36ae59dd29a54af9e0
Darya <cadinspect@gmail.com>
2021-07-19 22:50:31 +0300

    Добавлено взаимодействие с меню

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let menuBar = document.querySelector(".menuBar");
+let menuBtn = document.querySelector(".menu");
+
+/* Убираем зазор между рабочей областью и вкладкой меню "Файл", когда наводим на нее */
+menuBtn.addEventListener("mouseover", () => {
+    document.querySelector(".workspace").classList.add("topLeftCorner");
+})
+
+menuBtn.addEventListener("mouseout", () => {
+    document.querySelector(".workspace").classList.remove("topLeftCorner");
+})
+
+function toggleMenu(menu) {
+    menu.classList.toggle("active");
+}
+
+function menuItemClick(menuItem) {
+    
+}
+
+function menuClick(menu) {
+    let activeMenu = document.querySelector(".menu.active");
+    if (activeMenu != null && activeMenu != menu) {
+        toggleMenu(activeMenu);
+    }
+    toggleMenu(menu);
+}
+
+menuBar.addEventListener("click", () => {
+    let target = event.target;
+    let menu = target.closest(".menu");
+    if (target.classList == "menuItem") {
+        menuItemClick(target);
+        toggleMenu(menu);
+    } else {
+        menuClick(menu);
+    }
+})
+
+document.addEventListener("click", () => {
+    let target = event.target;
+    
+    /* Закрываем вкладку меню по клику вне строки меню */
+    let isMenuBar = (target == menuBar || menuBar.contains(target));
+    let activeMenu = document.querySelector(".menu.active");
+    if (!isMenuBar && activeMenu != null) {
+        toggleMenu(activeMenu);
+    }
+})
