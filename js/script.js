f7af24cb78b97544fe380f95900d3a16bff6f28f
vicadlsn <cadinspect@gmail.com>
2021-07-20 13:35:21 +0300

    Формы создания/изменения изображения
    - Формы можно открыть/закрыть
    - Добавлена валидация значений в input'ах (вроде)
    - Небольшие визуальные изменения

--- js/script.js
+++ js/script.js
@@ -18,12 +18,26 @@ menuBtn.addEventListener("mouseout", () => {
     document.querySelector(".workspace").classList.remove("topLeftCorner");
 })
 
+/* Меню */
+
 function toggleMenu(menu) {
     menu.classList.toggle("active");
 }
 
 function menuItemClick(menuItem) {
-    
+    toggleMenu(menuItem.closest(".menu"));
+    if (menuItem.id == "createImage") {
+        showForm(".imageCreationForm");
+    }
+    if (menuItem.id == "saveImage") {
+
+    }
+    if (menuItem.id == "imageSize") {
+        showForm(".imageResizeForm");
+    }
+    if (menuItem.id == "canvasSize") {
+        showForm(".canvasResizeForm");
+    }
 }
 
 function menuClick(menu) {
@@ -39,15 +53,23 @@ menuBar.addEventListener("click", () => {
     let menu = target.closest(".menu");
     if (target.classList == "menuItem") {
         menuItemClick(target);
-        toggleMenu(menu);
     } else {
         menuClick(menu);
     }
 })
 
+menuBar.addEventListener("mouseover", () => {
+    let target = event.target.closest(".menu");
+    let activeMenu = document.querySelector(".menu.active");
+    if (activeMenu != null && activeMenu != target) {
+        toggleMenu(activeMenu);
+        toggleMenu(target);
+    }
+})
+
 document.addEventListener("click", () => {
     let target = event.target;
-    
+
     /* Закрываем вкладку меню по клику вне строки меню */
     let isMenuBar = (target == menuBar || menuBar.contains(target));
     let activeMenu = document.querySelector(".menu.active");
@@ -55,3 +77,67 @@ document.addEventListener("click", () => {
         toggleMenu(activeMenu);
     }
 })
+
+/* Формы */
+
+window.addEventListener("load", () => {
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    let buttons = document.querySelectorAll("form > div > button");
+    for (let button of buttons) {
+        button.addEventListener("click", buttonClick);
+    }
+})
+
+function showForm(formClass) {
+    document.querySelector(".overlay").removeAttribute("hidden");
+    let form = document.querySelector(formClass);
+    form.classList.add("active");
+    setFormValues(form);
+}
+
+function closeForm(form) {
+    document.querySelector(".overlay").setAttribute("hidden", true);
+    document.querySelector(".form.active").classList.remove("active");
+}
+
+function setFormValues(form) {
+    let inputs = form.querySelectorAll("input");
+    inputs[0].value = canvasWidth; //width
+    inputs[1].value = canvasHeight; //height
+    if (activeForm.classList.contains("imageCreationForm")) {
+        inputs[2].value = canvasBackgroundColor; // background color
+        inputs[3].checked = canvasIsTransparent; // transparency
+    }
+}
+
+function inputIsValid() {
+    let form = document.querySelector(".form.active");
+    let inputs = form.querySelectorAll("input");
+    return (inputs[0].validity.valid && inputs[1].validity.valid);
+}
+
+function buttonClick() {
+    let button = event.target;
+    switch (button.name) {
+        case "cancel":
+            closeForm();
+        case "createImage":
+            if (inputIsValid()) {
+                closeForm();
+                createNewImage();
+            };
+        case "resizeImage":
+            if (inputIsValid()) {
+                closeForm();
+                resizeImage();
+            };
+        case "resizeCanvas":
+            if (inputIsValid()) {
+                closeForm();
+                resizeCanvas();
+            };
+    }
+}
+
