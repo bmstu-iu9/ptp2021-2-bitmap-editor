9c00b709d38686c0b127478e6f835584e22f65d7
vicadlsn <cadinspect@gmail.com>
2021-08-01 19:47:41 +0300

    Добавление изображения на холст
    - Можно изменить размер изображения
    - Можно переместить изображение

--- js/script.js
+++ js/script.js
@@ -21,6 +21,8 @@ window.addEventListener("load", () => {
     for (let button of buttons) {
         button.addEventListener("click", buttonClick);
     }
+    document.querySelector("button[name=insertImage]").addEventListener("click", insertImageButtonClick);
+    document.querySelector("button[name=cancelImageInsertion]").addEventListener("click", cancelImageInsertionClick);
 })
 
 /* Убираем зазор между рабочей областью и вкладкой меню "Файл", когда наводим на нее */
@@ -353,4 +355,295 @@ function hotkeyPress() {
                 break;
         }
     }
+}
+
+/* Поместить изображение на холст */
+
+document.querySelector("#insertImage").addEventListener("mouseup", () => {
+    document.querySelector(".menu").classList.remove("active");
+});
+
+document.querySelector("#insertImage").addEventListener("change", () => {
+    let file = event.target.files[0];
+    if (file) {
+        openImageToInsert(file);
+    }
+});
+
+function openImageToInsert(file) {
+    let reader = new FileReader();
+    let image = new Image();
+    image.onload = function () {
+        initImageInsertion(image);
+    }
+    reader.onload = function () {
+        image.src = reader.result;
+    }
+    reader.readAsDataURL(file);
+    document.querySelector("input[name=insertImage]").value = null;
+}
+
+let containerState = {
+        minWidth: 10,
+        minHeight: 10,
+        img: new Image(),
+    },
+    container, containerAngle, angle;
+
+function initImageInsertion(image) {
+    container = document.createElement("div");
+    container.classList = "selectionContainer";
+    canvasArea.append(container);
+
+    containerState.img.src = image.src;
+    container.insertAdjacentHTML("afterbegin", "<img src='" + image.src + "' id='selectedImage'>")
+
+    container.insertAdjacentHTML("afterbegin", "<span class='selectionHandle N'></span>")
+    container.insertAdjacentHTML("afterbegin", "<span class='selectionHandle E'></span>")
+    container.insertAdjacentHTML("afterbegin", "<span class='selectionHandle W'></span>")
+    container.insertAdjacentHTML("afterbegin", "<span class='selectionHandle NE'></span>")
+    container.insertAdjacentHTML("afterbegin", "<span class='selectionHandle NW'></span>")
+    container.insertAdjacentHTML("beforeend", "<span class='selectionHandle SE'></span>")
+    container.insertAdjacentHTML("beforeend", "<span class='selectionHandle SW'></span>")
+    container.insertAdjacentHTML("beforeend", "<span class='selectionHandle S'></span>")
+    container.insertAdjacentHTML("beforeend", "<span class='rotationHandle'></span>")
+
+    container.style.top = "50%";
+    container.style.left = "50%";
+
+    let handles = container.querySelectorAll(".selectionHandle");
+    for (let handle of handles) {
+        handle.addEventListener("mousedown", startResize);
+    }
+    //container.querySelector(".rotationHandle").addEventListener("mousedown", startRotation);
+
+    removeDocumentEvents();
+
+    document.querySelector("#selectedImage").addEventListener("mousedown", startMoving);
+    document.addEventListener("keydown", imageInsertionKeyPress);
+
+    containerAngle = 0;
+
+    document.querySelector(".palette").setAttribute("hidden", true);
+    document.querySelector(".insertImageButtons").removeAttribute("hidden");
+}
+
+function removeDocumentEvents() {
+    document.removeEventListener("keydown", hotkeyPress);
+    menuBar.removeEventListener("mousedown", menuBarClick);
+}
+
+function returnDocumentEvents() {
+    document.addEventListener("keydown", hotkeyPress);
+    menuBar.addEventListener("mousedown", menuBarClick);
+}
+
+function insertImage() {
+    //saveContainerState()
+    let image = document.querySelector("#selectedImage");
+    let x = container.offsetLeft - canvas.offsetLeft;
+    let y = container.offsetTop - canvas.offsetTop;
+    /*let centerX = containerState.left + containerState.width / 2;
+    let centerY = containerState.top + containerState.height / 2;
+    context.save();
+    context.translate(x + containerState.width / 2, y + containerState.height / 2);
+    context.rotate(containerAngle);
+    context.drawImage(image, -containerState.width / 2, -containerState.height / 2, image.width, image.height);*/
+    context.drawImage(image, x, y, image.width, image.height);
+    //context.restore();
+}
+
+function insertImageButtonClick() {
+    insertImage();
+    returnDocumentEvents();
+    container.remove();
+    document.removeEventListener("keydown", imageInsertionKeyPress);
+    document.querySelector(".palette").removeAttribute("hidden");
+    document.querySelector(".insertImageButtons").setAttribute("hidden", true);
+}
+
+function cancelImageInsertionClick() {
+    returnDocumentEvents();
+    container.remove();
+    document.removeEventListener("keydown", imageInsertionKeyPress);
+    document.querySelector(".palette").removeAttribute("hidden");
+    document.querySelector(".insertImageButtons").setAttribute("hidden", true);
+}
+
+function imageInsertionKeyPress() {
+    event.preventDefault();
+    event.stopPropagation();
+    switch (event.key) {
+        case "Enter":
+            insertImageButtonClick();
+            break;
+        case "Escape":
+            cancelImageInsertionClick();
+            break;
+        case "ArrowUp":
+            container.style.top = container.offsetTop - 1 + "px";
+            break;
+        case "ArrowRight":
+            container.style.left = container.offsetLeft + 1 + "px";
+            break;
+        case "ArrowDown":
+            container.style.top = container.offsetTop + 1 + "px";
+            break;
+        case "ArrowLeft":
+            container.style.left = container.offsetLeft - 1 + "px";
+            break;
+    }
+}
+
+function startResize() {
+    event.preventDefault();
+    event.stopPropagation();
+    saveContainerState();
+    document.addEventListener("mousemove", resizing);
+    document.addEventListener("mouseup", endResize);
+}
+
+function endResize() {
+    event.preventDefault();
+    document.removeEventListener("mousemove", resizing);
+    document.removeEventListener("mouseup", endResize);
+}
+
+function saveContainerState() {
+    containerState.width = container.clientWidth;
+    containerState.height = container.clientHeight;
+    containerState.left = container.offsetLeft;
+    containerState.top = container.offsetTop;
+    containerState.mouseX = event.clientX + workspace.scrollLeft;
+    containerState.mouseY = event.clientY + workspace.scrollTop;
+    containerState.event = event;
+}
+
+function resizing() {
+    let mouseX = event.clientX + workspace.scrollLeft - workspace.offsetLeft;
+    let mouseY = event.clientY + workspace.scrollTop - workspace.offsetTop;
+
+    let width, height, left, top;
+
+    let handleClassList = containerState.event.target.classList;
+
+    if (handleClassList.contains("SE")) {
+        width = mouseX - containerState.left;
+        if (event.shiftKey) {
+            height = width * containerState.img.height / containerState.img.width;
+        } else {
+            height = mouseY - containerState.top;
+        }
+        left = containerState.left;
+        top = containerState.top;
+
+    } else if (handleClassList.contains("SW")) {
+        width = containerState.width + containerState.left - mouseX;
+        if (event.shiftKey) {
+            height = width * containerState.img.height / containerState.img.width;
+        } else {
+            height = mouseY - containerState.top;
+        }
+        left = mouseX;
+        top = containerState.top;
+
+    } else if (handleClassList.contains("NW")) {
+        width = containerState.width + containerState.left - mouseX;
+        left = mouseX;
+        if (event.shiftKey) {
+            height = width * containerState.img.height / containerState.img.width;
+            top = containerState.top + containerState.height - height;
+        } else {
+            height = containerState.height + containerState.top - mouseY;
+            top = mouseY;
+        }
+
+    } else if (handleClassList.contains("NE")) {
+        width = mouseX - containerState.left;
+        left = container.offsetLeft;
+        if (event.shiftKey) {
+            height = width * containerState.img.height / containerState.img.width;
+            top = containerState.top + containerState.height - height;
+        } else {
+            height = containerState.height + containerState.top - mouseY;
+            top = mouseY;
+        }
+    } else if (handleClassList.contains("N")) {
+        height = containerState.height + containerState.top - mouseY;
+        top = mouseY;
+        if (event.shiftKey) {
+            width = height * containerState.img.width / containerState.img.height;
+            left = containerState.left - (width - containerState.width) / 2;
+        } else {
+            width = container.clientWidth;
+            left = container.offsetLeft;
+        }
+
+    } else if (handleClassList.contains("E")) {
+        width = mouseX - containerState.left;
+        left = container.offsetLeft;
+        if (event.shiftKey) {
+            height = width * containerState.img.height / containerState.img.width;
+            top = containerState.top - (height - containerState.height) / 2;
+        } else {
+            height = container.clientHeight;
+            top = container.offsetTop;
+        }
+
+    } else if (handleClassList.contains("S")) {
+        height = mouseY - containerState.top;
+        top = container.offsetTop;
+        if (event.shiftKey) {
+            width = height * containerState.img.width / containerState.img.height;
+            left = containerState.left - (width - containerState.width) / 2;
+        } else {
+            width = container.clientWidth;
+            left = container.offsetLeft;
+        }
+
+    } else if (handleClassList.contains("W")) {
+        width = containerState.width - (mouseX - containerState.left);
+        left = mouseX;
+        if (event.shiftKey) {
+            height = width * containerState.img.height / containerState.img.width;
+            top = containerState.top - (height - containerState.height) / 2;
+        } else {
+            height = container.clientHeight;
+            top = container.offsetTop;
+        }
+    }
+
+    if (width > containerState.minWidth && height > containerState.minHeight) {
+        resizeSelectedImage(width, height);
+        container.style.top = top + "px";
+        container.style.left = left + "px";
+    }
+}
+
+function resizeSelectedImage(width, height) {
+    let image = document.querySelector("#selectedImage");
+    image.style.width = width + "px";
+    image.style.height = height + "px";
+}
+
+function startMoving() {
+    event.preventDefault();
+    event.stopPropagation();
+    saveContainerState();
+    document.addEventListener("mousemove", moving);
+    document.addEventListener("mouseup", endMoving);
+}
+
+function endMoving() {
+    event.preventDefault();
+    document.removeEventListener("mouseup", endMoving);
+    document.removeEventListener("mousemove", moving);
+}
+
+function moving() {
+    let mouseX = event.clientX + workspace.scrollLeft;
+    let mouseY = event.clientY + workspace.scrollTop;
+
+    container.style.left = containerState.left + mouseX - containerState.mouseX + "px";
+    container.style.top = containerState.top + mouseY - containerState.mouseY + "px";
 }
