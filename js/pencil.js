c45e37d12772e23802f3c66601e5e072c9f52bdb
ynastt <yarovikovaanastasia1@gmail.com>
2021-08-22 09:47:43 +0400

    исправление карандаша

--- js/pencil.js
+++ js/pencil.js
@@ -1,27 +1,30 @@
-//"use strict";
-
-//let canvas = document.querySelector("#canvas");
-//let context = canvas.getContext("2d"); <- уже объявлено в script.js
-
 function pencilClick() {
+    var mouseIsDown = false;
 
-    context.lineCap = "round"; // скругленные концы линий
-    context.lineWidth = 2; //ширина
- 
-    // любое движение мышки по canvas вызывает эту функцию
-    canvas.onmousemove = function drawing(event) {
-      // в "e"  попадает экземпляр MouseEvent
-      var x = event.offsetX;
-      var y = event.offsetY;
-      var xdif = event.movementX;
-      var ydif = event.movementY;
- 
-      if (event.buttons > 0) {
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(x - xdif, y - ydif);
-        context.stroke();
-        context.closePath();
-      }
+    canvas.onmousedown = function (event) {
+
+        canvas.onmousemove = function drawing(event) {
+            // в "event"  попадает экземпляр MouseEvent
+            var x = event.offsetX;
+            var y = event.offsetY;
+            var xdif = event.movementX;
+            var ydif = event.movementY;
+
+            if (event.buttons > 0) {
+                context.beginPath();
+                context.moveTo(x, y);
+                context.lineTo(x - xdif, y - ydif);
+                context.stroke();
+                context.closePath();
+            }
+        }
+
+        mouseIsDown = true;
     }
+    canvas.onmouseup = function (e) {
+        if (mouseIsDown) pencilClick(e);
+
+        mouseIsDown = false;
+    }
+
 }
