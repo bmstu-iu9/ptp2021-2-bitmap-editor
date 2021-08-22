789f129b8c5426e54676a8605418dbf9739e6b94
ynastt <yarovikovaanastasia1@gmail.com>
2021-08-23 00:36:41 +0400

    нормальная работа линии со сменой основного цвета

--- js/line.js
+++ js/line.js
@@ -1,18 +1,23 @@
 function lineClick() {
-  context.lineCap = "round";
+    context.lineCap = "round";
 
-  var starter_x;
-  var starter_y;
+    var starter_x;
+    var starter_y;
 
-  canvas.onmousedown = function bb(event) {
-    starter_x = event.offsetX;
-    starter_y = event.offsetY;
-  };
-  canvas.onmouseup = function bbb(event) {
-    context.beginPath();
-    context.moveTo(starter_x, starter_y);
-    context.lineTo(event.offsetX, event.offsetY);
-    context.stroke();
-    context.closePath();
-  };
+    canvas.onmousedown = function bb(event) {
+        canvas.onmousemove = null;
+        starter_x = event.offsetX;
+        starter_y = event.offsetY;
+    };
+    canvas.onmouseup = function bbb(event) {
+        canvas.onmousemove = null;
+        context.beginPath();
+        context.moveTo(starter_x, starter_y);
+        context.lineTo(event.offsetX, event.offsetY);
+        context.strokeStyle = currentColor;
+        context.fillStyle = currentColor;
+        context.stroke();
+        context.closePath();
+    };
+    
 }
